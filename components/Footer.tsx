7fd8aca52bdcddd0ac8b15bbb126d8285bb847df
narenkumarbe <narenkumarbe@gmail.com>
2023-05-31 13:24:37 +0530

    final

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,10 @@
-import React from "react"
+import React from "react";
 import {
   AiOutlineGithub,
   AiOutlineTwitter,
   AiOutlineLinkedin,
   AiOutlineYoutube,
-} from "react-icons/ai"
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
@@ -12,10 +12,14 @@ const Footer = () => {
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
       <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
         <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
-          © 2023 Hosna Qasmei<a href="/" className="hover:underline"></a>
+          © 2023 Hari Prasath<a href="/" className="hover:underline"></a>
         </div>
         <div className="flex flex-row items-center justify-center space-x-2 mb-1">
-          <a href="https://github.com/hqasmei" rel="noreferrer" target="_blank">
+          <a
+            href="https://github.com/narenkumarbe"
+            rel="noreferrer"
+            target="_blank"
+          >
             <AiOutlineGithub
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
               size={30}
@@ -33,7 +37,7 @@ const Footer = () => {
           </a>
 
           <a
-            href="https://www.linkedin.com/in/hosnaqasmei/"
+            href="https://www.linkedin.com/in/hari-prasath-b810ba220"
             rel="noreferrer"
             target="_blank"
           >
@@ -42,11 +46,7 @@ const Footer = () => {
               size={30}
             />
           </a>
-          <a
-            href="https://www.youtube.com/channel/UCQBMkSDgbxDb8usMeXmOZyA"
-            rel="noreferrer"
-            target="_blank"
-          >
+          <a href="#" rel="noreferrer" target="_blank">
             <AiOutlineYoutube
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
               size={30}
@@ -55,7 +55,7 @@ const Footer = () => {
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
